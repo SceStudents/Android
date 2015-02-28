731d97576ffa66feeaea24e1068b724bd3defc40
David Eini <david.eini.se@gmail.com>
2015-02-28 05:12:50 +0200

    Add Location model Add DbContext model

---
+++ AndroidTester1.0/src/com/davideini/DataBase/DbContext.java
@@ -0,0 +1,16 @@
+package com.davideini.DataBase;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.davideini.models.location.Location;
+
+public class DbContext {
+	public DbContext() {
+
+	}
+	
+	public Collection<Location> Locations;
+}
--- AndroidTester1.0/src/com/davideini/models/User.java
+++ AndroidTester1.0/src/com/davideini/models/User.java
@@ -1,21 +1,24 @@
 package com.davideini.models;
 
+import com.davideini.DataBase.DbContext;
+import com.davideini.models.location.Location;
+
 public class User {
-	
+
 	// private variables
 	private String username;
-	
-	
+
 	public User() {
-		// TODO Auto-generated constructor stub
+		DbContext db = new DbContext();
+		
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 }
