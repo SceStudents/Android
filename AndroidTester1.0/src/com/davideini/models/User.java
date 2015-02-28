836e5a0e84baa7a16ed4690d7164612dcc9c1a88
David Eini <david.eini.se@gmail.com>
2015-02-28 04:51:55 +0200

    Modify User model

--- AndroidTester1.0/src/com/davideini/models/User.java
+++ AndroidTester1.0/src/com/davideini/models/User.java
@@ -1,5 +1,21 @@
 package com.davideini.models;
 
 public class User {
-
+	
+	// private variables
+	private String username;
+	
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 }
