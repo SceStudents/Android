77cbd7d943dc66be42df770b20fa6f8e90b83267
David Eini <david.eini.se@gmail.com>
2015-02-28 05:38:56 +0200

    Add Map activity

--- AndroidTester1.0/gen/com/davideini/androidtester/R.java
+++ AndroidTester1.0/gen/com/davideini/androidtester/R.java
@@ -29,18 +29,21 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_settings=0x7f030001;
-        public static final int activity_user=0x7f030002;
+        public static final int activity_map_settings=0x7f030001;
+        public static final int activity_settings=0x7f030002;
+        public static final int activity_user=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
-        public static final int settings=0x7f070001;
-        public static final int user=0x7f070002;
+        public static final int map_settings=0x7f070001;
+        public static final int settings=0x7f070002;
+        public static final int user=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_map_settings=0x7f050005;
         public static final int title_activity_settings=0x7f050003;
         public static final int title_activity_user=0x7f050004;
     }
